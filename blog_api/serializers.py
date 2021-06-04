ec13dbc6b475fd032250de237281ab8de82872fb
aktanaktan <aktan2222@gmail.com>
2021-06-04 20:56:43 +0600

    first commit

---
+++ blog_api/serializers.py
@@ -0,0 +1,115 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from blog_api.models import Post, Comment, Category, PostImages
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=6, write_only=True, required=True)
+    password2 = serializers.CharField(min_length=6, write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password2')
+
+
+
+
+    # validate - нужен для проверки(валидации)
+
+
+    def validate_first_name(self, value):
+        if not value.istitle():
+            raise serializers.ValidationError("Name must start with uppercase")
+        return value
+
+    def validate(self, attrs):
+        password2 = attrs.pop('password2')
+        if attrs['password'] != password2:
+            raise serializers.ValidationError('ПАРОЛЬ НЕ СОВПАДАЕТ')
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data.get('last_name')
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+
+# ____________________________________________________________________________________________________________________
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'parent')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.children.exists():
+            representation['children'] = CategorySerializer(instance=instance.children.all(), many=True).data
+        return representation
+
+
+# exists - проверяет есть или нет объекты
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImages
+        exclude = ('id',)
+
+
+# ___________________________________________________________________________________________________________________
+
+class PostSerializer(serializers.ModelSerializer):
+    # source --> через него получаем конкретно от кого берем посты
+    owner = serializers.ReadOnlyField(source='owner.username')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    category = CategorySerializer(many=False, read_only=True)
+    images = PostImageSerializer(many=True, read_only=False)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'body', 'owner', 'comments', 'category', 'preview', 'images')
+
+    def validate(self, attrs):
+        print(attrs)
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        # print('Файлы: ', request.FILES)
+        images_data = request.FILES
+        created_post = Post.objects.create(**validated_data)
+        print(created_post)
+        print('Work:', images_data.getlist('images'))
+        print('Is not work:', images_data)
+        # for image_data in images_data.getlist('images'):
+        #     PostImages.objects.create(post=created_post, image=image_data)
+        images_obj = [PostImages(post=created_post, image=image)
+                      for image in images_data.getlist('images')]
+        PostImages.objects.bulk_create(images_obj)
+        return created_post
+
+
+# serializer --- нужен для того, чтобы менять формат текста из query_set в dictionary и из него в json
+# ___________________________________________________________________________________________________________________
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'body', 'owner', 'post')
---
+++ blog_api/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django_filters import rest_framework as filters
+from rest_framework import permissions
+from rest_framework import generics
+from blog_api import serializers
+from django.contrib.auth.models import User
+from django.db import connection
+
+from blog_api.models import Post, Comment, Category
+from blog_api.permissions import IsOwnerOrReadOnly
+
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = serializers.RegisterSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.select_related('owner', 'category')
+    serializer_class = serializers.PostSerializer
+    filter_backends = (filters.DjangoFilterBackend, )
+    filterset_fields = ('title', 'category')
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        print(f'Queries Counted: {len(connection.queries)}')
+        return response
+
+
+class PostCreateView(generics.CreateAPIView):
+    serializer_class = serializers.PostSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    """Endpoint for Retrieve single post"""
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class PostDeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+
+
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+
+class CategoryView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = serializers.CategorySerializer
